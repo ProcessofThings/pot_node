de7962512e7bd7cdf16912022737b5231e333679
dexteruk <dexteruk@gmail.com>
2018-03-09 19:24:01 +0200

    Setup Single Process

--- lib/PotNode.pm
+++ lib/PotNode.pm
@@ -4,6 +4,7 @@ use Mojo::Base 'Mojolicious';
 # This method will run once at server start
 sub startup {
   my $self = shift;
+  my $redis = Mojo::Redis2->new;
 
   # Load configuration from hash returned by "my_app.conf"
   my $config = $self->plugin('Config');
@@ -11,13 +12,17 @@ sub startup {
   # Documentation browser under "/perldoc"
   $self->plugin('PODRenderer') if $config->{perldoc};
   $self->plugin('PotNode::Helpers');
+  $self->mode('development');
   
   $self->log->path('/home/node/log/pot_node.log');
   
-  Mojo::IOLoop->recurring(60 => sub {
+  Mojo::IOLoop->singleton->recurring(60 => sub {
+    if (!$redis->exists("checkprocess")){
+    $redis->set(checkprocess => "yes");
     use Mojo::UserAgent;
     ## recurring events
     my $loop = shift;
+    $loop->max_connections(1);
     my $path = "/home/node/.multichain/";
     my $process_chk_command;
     my $command;
@@ -25,35 +30,35 @@ sub startup {
     my $ua  = Mojo::UserAgent->new;
 
     ## Checks the multichain directory for any active blockchains and checks if the daemon is running
-    
     opendir( my $DIR, $path );
     while ( my $entry = readdir $DIR ) {
         ## Finds all directories and filters out all directories apart from those that contain HEX 32 chars
         next unless -d $path . '/' . $entry;
         next if $entry eq '.' or $entry eq '..';
         next if $entry !~ m/^\w{32}$/;
-        ## gets all process with exact match
-        $command = 'pgrep -f "^multichaind '.$entry.' -daemon$"';
-        $process_chk_command = qx/$command/;
-        ## Removes any \n\r
-        $process_chk_command =~ s/\R//g;
-        if ($process_chk_command ne '') {
-            $self->app->log->debug("Running Process : $process_chk_command");
+        if ( -f '/home/node/run/'.$entry.'.pid') {
+            $self->app->log->debug("Running Process : $entry");
         } else {
             ## launched the daemon using > /dev/null & to return control to mojolicious
-            $command = "multichaind $entry -daemon > /dev/null &";
+            $command = "multichaind $entry -daemon -pid=/home/node/run/$entry.pid > /dev/null &";
             system($command);
             $self->app->log->debug("Starting : $entry");
         }
     }
-    closedir $DIR;
     
-    $command = 'ipfs add -r -w -Q /home/node/pot_node';
-    my $value = qx/$command/;
-    $value =~ s/\R//g;
-    $self->app->log->debug("pot_node Hash : $value");
-    #my $res = $ua->get("http://127.0.0.1:5001/api/v0/pubsub/sub?arg=$value&discover=\1");
-    #$self->app->dumper($res);
+    closedir $DIR;
+    if (!$redis->exists("addpotnode")){
+        $command = 'ipfs add -r -w -Q /home/node/pot_node';
+        my $value = qx/$command/;
+        $value =~ s/\R//g;
+        $self->app->log->debug("pot_node Hash : $value");
+        #my $res = $ua->get("http://127.0.0.1:5001/api/v0/pubsub/sub?arg=$value&discover=\1");
+        #$self->app->dumper($res);
+        $redis->set(addpotnode => "yes");
+    }
+
+    $redis->del("checkprocess");
+    }
   });
 
   # Router
